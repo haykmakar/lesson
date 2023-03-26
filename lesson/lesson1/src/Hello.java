0057c4f51e27f8128f6bc953063af057c30c0f5c
haykmakar <makaryanhayk7@gmail.com>
2023-03-26 14:30:13 +0400

    My first git commit

---
+++ lesson/lesson1/src/Hello.java
@@ -0,0 +1,19 @@
+public class Hello
+{
+    public static void main(String[] args) {
+        System.out.println("barev");
+        System.out.println("inchx es Hayk jan");
+        float y = 767427f;
+        System.out.println(y);
+        int b =524626;
+        byte a = 5;
+        System.out.println(a+b);
+        String i = "Hayk";
+        String p = "Makaryan";
+        System.out.println(i + " " + p);
+        System.out.println("y = " + y);
+        System.out.println("a+b=" + a+b);
+
+        }
+    }
+
